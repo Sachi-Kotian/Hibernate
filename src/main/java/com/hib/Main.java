5dd2fd539c0ae17f3406ccf282748243cc27ac0e
Sachi <kotiansachi@gmail.com>
2026-07-02 23:38:21 +0530

    Data saved in database

--- src/main/java/com/hib/Main.java
+++ src/main/java/com/hib/Main.java
@@ -1,8 +1,34 @@
 package com.hib;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String args[])
     {
 
+        Student s1=new Student();
+        s1.setRoll(1);
+        s1.setName("John");
+        s1.setAge(20);
+
+        Configuration cfg=new Configuration();
+        cfg.addAnnotatedClass(com.hib.Student.class);
+        cfg.configure();
+
+        cfg.setProperty
+                ("hibernate.connection.password",
+                        System.getenv("DB_password"));
+
+        SessionFactory fs=cfg.buildSessionFactory();
+        Session session=fs.openSession();
+
+        Transaction ts=session.beginTransaction();
+        session.persist(s1);
+        ts.commit();
+
+        System.out.println(s1);
     }
 }
---
+++ src/main/java/com/hib/Student.java
@@ -0,0 +1,46 @@
+package com.hib;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    int roll;
+    String name;
+    int age;
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "roll=" + roll +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
